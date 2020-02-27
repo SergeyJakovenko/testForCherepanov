b1ef777e806a6f1a99faa3eb56ea4dd51d126453
Сергей Яковенко <jakovenko@opvf.ru>
2020-02-27 11:34:37 +0300

    Седьмой коммит

--- mouse.cpp
+++ mouse.cpp
@@ -23,7 +23,7 @@ static qreal normalizeAngle(qreal angle)
 //! [0]
 Mouse::Mouse()
     : angle(0), speed(0), mouseEyeDirection(0),
-      color(QRandomGenerator::global()->bounded(126), QRandomGenerator::global()->bounded(236), QRandomGenerator::global()->bounded(126))
+      color(QRandomGenerator::global()->bounded(126), QRandomGenerator::global()->bounded(216), QRandomGenerator::global()->bounded(125))
 
 {
     setTransform(QTransform().rotate(QRandomGenerator::global()->bounded(360 * 16)), true);
